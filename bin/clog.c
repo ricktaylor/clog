5becdf5960319a53f19dd1f8145d34218f863044
Rick Taylor <rick@tropicalstormsoftware.com>
2013-08-09 15:40:15 +0100

    A load of fixes
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- bin/clog.c
+++ bin/clog.c
@@ -1,8 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+int parse(int (*rd_fn)(void* p, unsigned char* buf, size_t* len), void* param);
 
 void* clog_malloc(size_t s)
 {
 	return malloc(s);
+}
+
+void* clog_realloc(void* p, size_t s)
+{
+	return realloc(p,s);
 }
 
 void clog_free(void* p)
@@ -10,7 +20,27 @@ void clog_free(void* p)
 	free(p);
 }
 
+static int read_fn(void* p, unsigned char* buf, size_t* len)
+{
+	*len = fread(buf,1,*len,(FILE*)p);
+	if (*len == 0 && ferror((FILE*)p))
+		return -1;
+
+	return 0;
+}
+
 int main(int argc, char* argv[])
 {
+	FILE* f = fopen(argv[1],"r");
+	if (!f)
+	{
+		printf("Failed to open %s: %s\n",argv[1],strerror(errno));
+		return -1;
+	}
+
+	parse(&read_fn,f);
+
+	fclose(f);
+
 	return 0;
 }
